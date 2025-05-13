70c84d36add2312b996424df97138f5e712fb957
Mohamed Ashik . A <118453656+Ashik-50@users.noreply.github.com>
2025-05-13 19:50:20 +0530

    Update Signup.js

--- src/components/User/Pages/Loginandsignup/Signup.js
+++ src/components/User/Pages/Loginandsignup/Signup.js
@@ -15,7 +15,7 @@ function Register() {
     event.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:8080/login', {
+      const response = await fetch('https://caresphere-backend.onrender.com/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
